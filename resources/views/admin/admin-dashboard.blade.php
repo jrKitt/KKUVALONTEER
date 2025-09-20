46d1cf5a62451b439a7f129aecd4b9e4454b7ae5
bbsw <sorawitthassadorn15@gmail.com>
2025-09-20 22:28:03 +0700

    update; (admin) dashboard

--- resources/views/admin/admin-dashboard.blade.php
+++ resources/views/admin/admin-dashboard.blade.php
@@ -6,10 +6,20 @@
 
 @section('layout-content')
     <div class="min-h-screen bg-gray-50">
-
-            <main>
-               admin dashboard
-            </main>
+        <header class="max-w-6xl mx-auto">
+            <div class="flex gap-5 p-4 h-fit max-md:flex-col">
+                <div class="border w-full rounded h-50">
+                    graph1
+                </div>
+                <div class="border w-full h-50 rounded">
+                    graph2
+                </div>
+            </div>
+            <hr class="my-6 text-gray-400">
+        </header>
+        <main>
+            admin dashboard
+        </main>
         </section>
     </div>
 @endsection
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -10,10 +10,18 @@
 </head>
 
 <body>
-    <div class="h-screen flex items-center justify-center font-extrabold text-9xl underline">
-        <div class="flex flex-col items-center">
+    <div class="h-screen flex flex-col items-center justify-center font-extrabold text-9xl ">
+        <div class="flex flex-col items-center underline">
             <img src="{{ asset('/images/family.png') }}" alt="img" class="w-200 rounded-4xl">
             <div>ADMIN</div>
+        </div>
+        <div class="text-3xl flex flex-col m-10 font-medium gap-2">
+            <button class="btn bg-red-300 text-zinc-600 transition-all hover:scale-200">
+                <a href="admin/dashboard">Dashboard</a>
+            </button>
+            <button class="btn bg-yellow-200 text-zinc-600 transition-all hover:scale-200">
+                <a href="admin/event">Event</a>
+            </button>
         </div>
     </div>
 </body>
