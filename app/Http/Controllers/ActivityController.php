4a8ba0132d280da6aca82ac5e540d095e111f141
AekkarinDEV <aekkarin.work@gmail.com>
2025-10-07 00:58:22 +0700

    update admin activities

--- app/Http/Controllers/ActivityController.php
+++ app/Http/Controllers/ActivityController.php
@@ -10,13 +10,15 @@ class ActivityController extends Controller
     
     //
     public function createActivity(Request $req) {
-        $activity_name = $req->input("activity_name");
-        error_log($activity_name);
+        error_log($req->input("start_time"));
         $new_activity = new Activity();
-        $new_activity->name_th = $activity_name;
+        $new_activity->name_th = $req->input("activity_name");
         $new_activity->description = $req->input("des");
         $new_activity->location = $req->input("location");
-        $new_activity->accept_amount = 0;
+        $new_activity->start_time = $req->input("start_time");
+        $new_activity->end_time = $req->input("end_time");
+        $new_activity->accept_amount = $req->input("accept_amount");
+        $new_activity->total_hour = $req->input("total_hour");
         $new_activity->create_by = 0;
         $new_activity->save();
         return redirect("/admin/event");
